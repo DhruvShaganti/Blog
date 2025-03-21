5cd2cb942af40801ee7181590ce80e504c3fa85c
DhruvShaganti <150896263+DhruvShaganti@users.noreply.github.com>
2025-03-21 10:27:59 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,34 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: import.meta.env.VITE_API_URL,
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// });
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, // Correct way to access environment variable
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
